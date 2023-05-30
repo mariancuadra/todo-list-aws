2487fdf0d156464502a715b973bae3c722c10412
Marian Cuadra <marian.cuadra.17@gmail.com>
2023-05-30 16:23:40 +0000

    cambiadas opciones de coverage

--- pipelines/PIPELINE-FULL-STAGING/unit_test.sh
+++ pipelines/PIPELINE-FULL-STAGING/unit_test.sh
@@ -7,6 +7,6 @@ echo "PYTHONPATH: $PYTHONPATH"
 export DYNAMODB_TABLE=todoUnitTestsTable
 python test/unit/TestToDo.py
 pip show coverage
-coverage run --include=src/todoList.py -m test/unit/TestToDo.py
-coverage report
+coverage run --include=src/todoList.py test/unit/TestToDo.py
+coverage report -m
 coverage xml
